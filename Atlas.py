b8390dc9a06a08b8a0606aa042f6fac6a19365eb
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-09 16:12:30 -0500

    picturetest

---
+++ Atlas.py
@@ -0,0 +1,32 @@
+import cv2
+
+#States for FSM
+#Sweep to find number of players
+Sweep = True
+Look = False
+Lock = False
+Tilt = False
+Wait = False
+#Soham's Classes
+#Ethics HRI core class
+#Probablistic Core
+#ME 123
+
+
+
+while Sweep:
+    if(Sweep):
+        Sweep = False
+        Look = True
+    if(Look):
+        Look = False
+        Lock = True
+    if(Lock):
+        Lock = False
+        Tilt = True
+    if(Tilt):
+        Tilt = False
+        Wait = True
+    if(Wait):
+        Wait = False
+        Look = True
