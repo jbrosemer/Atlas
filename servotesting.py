fcb41f165ea8a23def75bda005dcb2c9108376c7
jbrosemer <jordynbrosemer@gmail.com>
2021-11-16 18:43:47 +0000

    pyc removal

--- servotesting.py
+++ servotesting.py
@@ -1,5 +1,20 @@
 from adafruit_servokit import ServoKit
+import time
 kit = ServoKit(channels=16)
 
+servo=kit.servo[1]
 while True:
-    kit.servo[0].angle = (100)
+	t1=int(input('Enter servo angle:'))
+	servo.angle = t1
+	time.sleep(10)
+	servo.angle = 90
+
+#t1_servo= kit.servo[0]
+#t2_servo= kit.servo[1]
+
+#while True:
+#    t1=int(input('Enter servo angle for servo 1: '))
+#    t1_servo.angle=t1
+#     t2=int(input('Enter servo angle for servo 2: '))
+#     t2_servo.angle=t2
+    
