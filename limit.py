a2a8bc839f4829d853c76b84b4d3280e40b46e20
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-16 15:16:45 -0500

    changed limit

--- limit.py
+++ limit.py
@@ -3,5 +3,6 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 while True: # Run forever
+    print("hi")
     if GPIO.input(12) == GPIO.HIGH:
         print("Button was pushed!")
