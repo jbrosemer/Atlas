25dd210856f5d3e10826416a303213cd59177a60
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-16 14:21:08 -0500

    old commit

--- servotesting.py
+++ servotesting.py
@@ -1,20 +1,5 @@
 from adafruit_servokit import ServoKit
-import time
 kit = ServoKit(channels=16)
 
-servo=kit.servo[1]
 while True:
-	t1=int(input('Enter servo angle:'))
-	servo.angle = t1
-	time.sleep(10)
-	servo.angle = 90
-
-#t1_servo= kit.servo[0]
-#t2_servo= kit.servo[1]
-
-#while True:
-#    t1=int(input('Enter servo angle for servo 1: '))
-#    t1_servo.angle=t1
-#     t2=int(input('Enter servo angle for servo 2: '))
-#     t2_servo.angle=t2
-    
+    kit.servo[0].angle = (100)
