586a70490320e2b709c9c9f1f17cb514201d2f45
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-15 22:11:53 -0500

    confused

--- serial_test.py
+++ serial_test.py
@@ -1,10 +1,9 @@
-from serial import Serial
+import serial
 
-ser = Serial('/dev/ttyUSB0', 9600)
+ser = serial.Serial('/dev/ttyUSB0', 9600)
 ser.flush()
 
 while True:
     read_serial = ser.readline()
     read_serial = read_serial.decode("utf-8")
     print(read_serial)
-    
