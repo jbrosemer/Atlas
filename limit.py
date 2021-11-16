cb01ee2e4eec40ab788c873c1abcc126e06abb6a
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-16 15:23:39 -0500

    hi

--- limit.py
+++ limit.py
@@ -1,8 +1,5 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-while True: # Run forever
-    print("hi")
-    if GPIO.input(12) == GPIO.HIGH:
-        print("Button was pushed!")
+from gpizero import Button
+button = Button(12)
+while True:
+    if button.is_pressed:
+        print("hello")
