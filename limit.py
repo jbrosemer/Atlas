38645cf1426148b0101165c15ddc8c7abcfe2656
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-16 15:34:17 -0500

    changed button

--- limit.py
+++ limit.py
@@ -1,7 +1,5 @@
 from gpiozero import Button
 button = Button(12)
 while True:
-    if button.is_pressed:
-        print("hello")
-    else:
-        print("hi")
+    if ~button.is_pressed:
+        print("BAM")
