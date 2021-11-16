cc240f0252dea4ee728303b51ca1fa8253164ee4
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-16 15:06:25 -0500

    limit

--- limit.py
+++ limit.py
@@ -1,12 +1,10 @@
-import RPi.GPIO as GPIO
-import time
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+def button_callback(channel):
+    print("Button was pushed!")
 
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-while True:
-    input_state = GPIO.input(18)
-    if input_state == False:
-        print('Button Pressed')
-        time.sleep(0.2)
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+message = input("Press enter to quit\n\n") # Run until someone presses enter
+GPIO.cleanup() # Clean up
