2c8f5a4a46ede5d965a6715cacf44d2c1563c677
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-17 23:00:39 -0500

    da

--- servotesting.py
+++ servotesting.py
@@ -20,7 +20,7 @@ try:
         if CW:
             kit.servo[0].angle = (87)
         if CCW:
-            kit.servo[0].angle = (93)
+            kit.servo[0].angle = (95)
 except KeyboardInterrupt:
     end = time.time()
     print("time of run: ", start-end)
