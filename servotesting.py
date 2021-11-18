e6e64ea6dbf6c03b96e4ea5e0437550a2eeccc49
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-17 21:10:39 -0500

    servo changes again

--- servotesting.py
+++ servotesting.py
@@ -1,10 +1,22 @@
 import time
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
+CW = True
+CCW = False
 try:
     start = time.time()
     while True:
+        if ((time.time() - start)*10)%17 == 0:
+            if CW:
+                CW = False
+                CCW = True
+            else:
+                CCW = False
+                CW = True
+    if CW:
         kit.servo[0].angle = (100)
+    if CCW:
+        kit.servo[0].angle = (80)
 except KeyboardInterrupt:
     end = time.time()
     print("time of run: ", start-end)
