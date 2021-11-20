a53cfab05b247b2187033a513c44af13a08c66d0
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-20 17:05:30 -0500

    pitch

--- servotesting.py
+++ servotesting.py
@@ -18,7 +18,7 @@ try:
                 CCW = False
                 CW = True
         if CW:
-            kit.servo[0].angle = (84)
+            kit.servo[0].angle = (80)
         if CCW:
             kit.servo[0].angle = (94)
 except KeyboardInterrupt:
