5550a99803e26bebcac8a214c8fae17e33cde2f0
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-20 18:00:59 -0500

    new

--- servotesting.py
+++ servotesting.py
@@ -6,20 +6,30 @@ CCW = False
 try:
     start = time.time()
     while True:
-        if time.time()-start > 3.3:
-            start = time.time()
-            if CW:
-                CW = False
-                CCW = True
-            else:
-                CCW = False
-                CW = True
+        if CCW:
+            if time.time()-start > 7.01:
+                start = time.time()
+                if CW:
+                    CW = False
+                    CCW = True
+                else:
+                    CCW = False
+                    CW = True
+        elif CW:
+            if time.time()-start > 7.8:
+                start = time.time()
+                if CW:
+                    CW = False
+                    CCW = True
+                else:
+                    CCW = False
+                    CW = True
         if CW:
             print("cw")
-            kit.servo[0].angle = (73)
-        if CCW:
+            kit.servo[0].angle = (74)
+        elif CCW:
             print("ccw")
-            kit.servo[0].angle = (85)
+            kit.servo[0].angle = (82)
 except KeyboardInterrupt:
     end = time.time()
     print("time of run: ", start-end)
