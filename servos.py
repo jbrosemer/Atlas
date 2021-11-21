96bd30e84ee5fb83ffd066cb0b86e6940e9dd8e2
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-20 20:02:33 -0500

    two servos

--- servos.py
+++ servos.py
@@ -4,6 +4,8 @@ kit = ServoKit(channels=16)
 start = 0
 while True:
     angle = input("enter the motor speed: ")
+    angle2 = input("enter second motor")
     print(str(time.time()-start))
     start = time.time()
     kit.servo[1].angle = (int(angle))
+    kit.servo[2].angle = (int(angle2))
