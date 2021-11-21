4777249ec4353b5dfebf3dc8b472900737f4d172
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-21 17:44:18 -0500

    overrated

--- testing.py
+++ testing.py
@@ -107,9 +107,9 @@ try:
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             if (2*x + w)/2 > (width/2+25):
-                kit.servo[0].angle = (87)
-            elif (2*x + w)/2 < (width/2-25):
                 kit.servo[0].angle = (94)
+            elif (2*x + w)/2 < (width/2-25):
+                kit.servo[0].angle = (87)
             else:
                 kit.servo[0].angle = (90)
                 Drop = True
