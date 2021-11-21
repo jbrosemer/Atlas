f3ed4de432359b10c33c3c99f46ee0476fc04973
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-20 20:18:15 -0500

    punk

--- servos.py
+++ servos.py
@@ -2,10 +2,13 @@ import time
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 start = 0
+prevangle = 0
 while True:
+    prevangle = angle
     angle = input("enter the motor speed: ")
     angle2 = 90 + (90 - int(angle))
     print(str(time.time()-start))
     start = time.time()
-    kit.servo[1].angle = (int(angle))
-    kit.servo[2].angle = (int(angle2))
+    if prevangle != angle:
+        kit.servo[1].angle = (int(angle))
+        kit.servo[2].angle = (int(angle2))
