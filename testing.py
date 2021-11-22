6030562458c034feebd41681cb8cc0e722d93b6d
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-11-21 19:36:15 -0500

    atlas

--- testing.py
+++ testing.py
@@ -114,9 +114,9 @@ try:
             for (x, y, w, h) in faces:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 if (2*x + w)/2 > (width/2+30):
-                    kit.servo[0].angle = (88)
+                    kit.servo[0].angle = (92)
                 elif (2*x + w)/2 < (width/2-30):
-                    kit.servo[0].angle = (93)
+                    kit.servo[0].angle = (87)
                 else:
                     kit.servo[0].angle = (90)
                     Drop = True
