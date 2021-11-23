fe4ae26e5d78b0fae9340c9d27a85ef9ca90951e
foufei <jiayuki@outlook.com>
2021-11-22 20:33:01 -0500

    write date debug

--- serial_test.py
+++ serial_test.py
@@ -6,14 +6,14 @@ from os.path import join
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 ser.flush()
 
-start = time()
+start = time.time()
 with open(join("logs", datetime.strftime(datetime.now(), "%d_%m_%Y_%H:%M:%S_logs")),'w') as f:
     while True:
         read_serial = ser.readline()
         read_serial = read_serial.decode("utf-8")
         strs = read_serial.split(" ")
         if len(strs) > 3:
-            time = time()-start
+            time = time.time()-start
             lidar = int(strs[5])
             if lidar < 40:
                 print("Ball!")
